af946bda43ad3c3b1f85f3aeef6973e34abbfbce
Jmactiag <jessie.macatiag@gmail.com>
2018-03-08 15:47:08 -0800

    new init to new repo

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Office Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bic Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Parlor Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('DHaven Burger', false);
