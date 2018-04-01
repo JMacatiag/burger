7bcb4d646b5a1fd6a58929ad57921bc126fe1b5a
Jmactiag <jessie.macatiag@gmail.com>
2018-04-01 12:46:06 -0700

    connection edit for heroku

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 var mysql = require("mysql");
 
 
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+	connection = mysql.createConnection({
+	  host: process.env.HOST,
+	  user: process.env.USER,
+	  password: process.env.PASSWORD,
+	  database: process.env.DATABASE
+	});
+}
 
 // Make connection.
 connection.connect(function(err) {
